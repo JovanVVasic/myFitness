a0f796cbba2be9e0e0a4b25372373ebf1b85bdd1
JovanVVasic <jovan.vasic.54.20@ict.edu.rs>
2021-12-11 15:10:36 +0100

    a

--- js/main.js
+++ js/main.js
@@ -4,3 +4,65 @@ window.addEventListener('scroll', function(){
 
     header.classList.toggle('scrolling-active', windowPosition);
 })
+
+let begin = document.querySelector("#begin");
+let promise = document.querySelector("#promise");
+let strong = document.querySelector("#strong");
+let packages = document.querySelector("#packages");
+let container = document.querySelector("#conainer");
+
+const section = [begin,promise,strong,packages,container];
+const navLi = document.querySelectorAll('header nav ul li');
+
+window.addEventListener('scroll', ()=>{
+    let current='';
+
+    section.forEach(section => {
+        const secTop = section.offsetTop;
+        const secHeight = section.clientHeight;
+        if (pageYOffset >= secTop - secHeight / 3 ){
+            current=section.getAttribute('id');
+            
+        }
+
+        navLi.forEach(li =>{
+            li.classList.remove("active");
+            if (li.classList.contains(current)){
+                li.classList.add("active");
+            }
+        })
+
+    })
+})
+
+
+
+
+let box = document.getElementsByClassName("shadow");
+
+for (let i =0; i< box.length; i++){
+    box[i].addEventListener("mouseover", ()=>{
+        box[i].classList.add("b-shadow");
+    })
+    box[i].addEventListener("mouseout", ()=>{
+        box[i].classList.remove("b-shadow");
+    }
+    )
+}
+
+$(document).ready(function(){
+    $('.your-class').slick({
+        autoplay: true,
+        autoplaySpeed:2000,
+        arrows: false
+    });
+  });
+
+
+
+
+
+
+
+
+
